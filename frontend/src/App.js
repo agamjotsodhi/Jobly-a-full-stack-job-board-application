4c0cde190306f7665c7d7c83d0b8b50930439b11
Agamjot Sodhi <agamjotsodhi@hotmail.com>
2024-11-26 15:06:14 -0500

    wrote out api helper.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter } from 'react-router-dom';
+import JoblyRoutes from './Routes';
+import NavBar from './NavBar';
+import JoblyApi from './apiHelper';
+import { useEffect, useState } from 'react';
+import CurrentUserContext from './CurrentUserContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//  Use state scenarios 
+  const [ applications, setApplications ] = useState();
+	const [ currentUser, setCurrentUser ] = useState();
+	const [ storedValue, setValue ] = useLocalStorage();  
 }
 
 export default App;
